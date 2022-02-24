716935e1646419d3356352335ea4fb4f66953eec
Anh  Tuấn <tuananh28@MacBook-Pro-cua-Anh.local>
2022-02-24 15:59:27 +0700

    update branhc developemnt

--- initial/src/main/java/com/example/springboot/HelloController.java
+++ initial/src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot! version 4";
+		return "Greetings from Spring Boot! version 5";
 	}
 
 }
